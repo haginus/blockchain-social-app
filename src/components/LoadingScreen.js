0b55395d2415b99b6e0da55021519ecd61646d96
Haginus <hagiandrei.ah@gmail.com>
2021-05-11 22:21:35 +0300

    made loading screen absolute

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -2,7 +2,7 @@ import { Spinner } from 'react-bootstrap'
 
 function LoadingScreen() {
     return (
-        <div className="fullscreen center-hv">
+        <div className="fullscreen abs top-left center-hv" style={{background: 'rgba(255, 255, 255, 0.3)'}}>
             <Spinner animation="grow" variant="success"/>
         </div>
     )
