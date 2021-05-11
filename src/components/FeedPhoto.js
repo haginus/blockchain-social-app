c9daac9210460a57cfd981b00cc3efe3fe1b1341
Haginus <hagiandrei.ah@gmail.com>
2021-05-11 23:10:57 +0300

    like photos and follow users

--- src/components/FeedPhoto.js
+++ src/components/FeedPhoto.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-import { Heart } from 'react-bootstrap-icons';
+import { Heart, HeartFill } from 'react-bootstrap-icons';
 
-import { useRecoilState } from 'recoil';
-import { usersCache } from "../recoil/atoms";
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { appState, usersCache } from "../recoil/atoms";
 import { social } from '../Social';
 
 import './FeedPhoto.css';
 
-function FeedPhoto({ id, hash, description, likeCount, authorId }) {
+function FeedPhoto({ id, hash, description, likeCount, authorId, authorAddress }) {
 
     const [canFollow, setCanFollow] = useState(false);
     const [hasLiked, setHasLiked] = useState(null);
@@ -15,6 +15,10 @@ function FeedPhoto({ id, hash, description, likeCount, authorId }) {
     const [users, setUsers] = useRecoilState(usersCache);
 
     const [author, setAuthor] = useState({});
+
+    const [likeCountS, setLikeCount] = useState(likeCount);
+
+    const [app, setApp] = useRecoilState(appState);
 
     useEffect(() => {
         async function getUser() {
@@ -28,9 +32,45 @@ function FeedPhoto({ id, hash, description, likeCount, authorId }) {
                 setUsers({ ...users, [authorId]: userData });
                 setAuthor(userData);
             }
+
+            if(app.currentUser.id === authorId) {
+                setCanFollow(false);
+            } else {
+                // check if user has followed author (needs contract changes)
+                setCanFollow(true);
+            }
+
+            setHasLiked(await social.methods.photoLikedBy(id, app.selectedAccount).call());
         };
         getUser();
     }, [])
+
+    const likePhoto = () => {
+        if(hasLiked) {
+            return;
+        }
+        social.methods.likePhoto(id).send({ from: app.selectedAccount }).on('transactionHash', (hash) => {
+            setLikeCount(+likeCount + 1);
+            setHasLiked(true);
+
+        }).on('error', e => {
+            
+        });
+    }
+
+    const followAuthor = () => {
+        social.methods.followUser(authorAddress).send({ from: app.selectedAccount })
+        .on('transactionHash', (hash) => {
+            setCanFollow(false);
+            let newApp = { ...app };
+            newApp.currentUser.followingCount = +newApp.currentUser.followingCount + 1;
+            setApp(newApp);
+            setAuthor({ ...author, followerCount: +author.followerCount + 1 });
+            setUsers({ ...users, [authorId]: author });
+        }).on('error', e => {
+            
+        });
+    };
 
     return (
         <div className="feed-photo">
@@ -41,19 +81,22 @@ function FeedPhoto({ id, hash, description, likeCount, authorId }) {
                 <div className="username">
                     { author.username }
                 </div>
-                <div className="follow-button">Follow</div>
+                { canFollow ? 
+                    <div className="follow-button" onClick={() => followAuthor()}>Follow</div>
+                    : '' 
+                }
             </div>
             <div className="image-container">
                 <img src={`https://ipfs.infura.io/ipfs/${hash}`}/>
             </div>
             <div className="photo-footer">
                 <div className="photo-actions">
-                    <div className="icon-button">
-                        <Heart size={24}></Heart>
+                    <div className="icon-button" onClick={() => likePhoto()}>
+                        { hasLiked ? <HeartFill size={24}></HeartFill> : <Heart size={24}></Heart>}
                     </div>
                 </div>
                 <div className="photo-description-container">
-                    <div className="like-count bold">{ likeCount } likes</div>
+                    <div className="like-count bold">{ likeCountS } likes</div>
                     <div>
                         <span className="bold">haginus</span>
                         <span className="photo-description">{ description }</span>
