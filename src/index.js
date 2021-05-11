31c214fdb7be57fac3fca626dcaa168152663f58
Haginus <hagiandrei.ah@gmail.com>
2021-05-11 18:05:40 +0300

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+import { RecoilRoot } from 'recoil';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
